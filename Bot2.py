5fba07c481f9c1f46a6e7df079e2cf89707bc198
Vitalygorkov <vet55omsk@gmail.com>
2021-03-25 22:53:47 +0500

    bot2 ref

--- Bot2.py
+++ Bot2.py
@@ -11,6 +11,27 @@ import time
 
 bot = telebot.TeleBot(api_key)
 
+keyboard1 = telebot.types.ReplyKeyboardMarkup()
+keyboard1.row('баланс', '/start')
+
+# @bot.message_handler(content_types=['text'])
+# def start(message):
+#     if message.text.lower() == 'go':
+#         bot.send_message(message.from_user.id, 'начинаем эффективную работу', reply_markup=keyboard1)
+#         print('Баланс')
+#
+#     else:
+#         print(message.text)
+#         bot.send_message(message.from_user.id, 'Минуты пробел задача', reply_markup=keyboard1)
+#
+#     # elif message.text.lower().split()[0].isdigit() == True:
+#     #     bot.send_message(message.from_user.id, 'задание '+ message.text, reply_markup=keyboard1)
+#     #     print('задание')
+#     #
+#     # else:
+#     #     print(message.text)
+#     #     bot.send_message(message.from_user.id, 'Минуты пробел задача', reply_markup=keyboard1)
+
 @bot.message_handler(commands=['start'])
 def start_command(message):
     bot.send_message(
